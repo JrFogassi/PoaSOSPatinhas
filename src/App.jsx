e1148fa41f7f43434de50c7810917892ea835917
JrFogassi <jrfogassi@gmail.com>
2022-06-27 22:44:07 -0300

    the birth of the SOS Patinhas App, coding the first title

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import HeaderContainer from './components/Header'
+
+function App() {
+  return (
+    <div>
+        <HeaderContainer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeaderContainer = () =>{
+    return (
+        <div className="headerContainer">
+            <h1> S.O.S Patinhas POA </h1>
+        </div>
+    )
+}
+
+export default HeaderContainer
